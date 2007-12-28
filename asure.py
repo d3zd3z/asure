d20207a1747e8787055492d602fb0925815dd57d
David Brown <asure@davidb.org>
2007-12-27 21:42:09 -0800

    Write primitive tree comparison.
    Detects created files and directories.

--- asure.py
+++ asure.py
@@ -58,6 +58,116 @@ def walker(path, name, topstat):
     # Last, yield the leaving.
     yield ('u',)
 
+def compare_trees(prior, cur):
+    """Compare two scanned trees."""
+    a = prior.next()
+    if a[0] != 'd':
+	raise "Scan doesn't start with a directory"
+    b = cur.next()
+    if b[0] != 'd':
+	raise "Tree walk doesn't start with a directory"
+
+    # We don't concern ourselves with whether the names are the same
+    # at this point.
+    comp_walk(prior, cur)
+
+def comp_walk(prior, cur, depth=1):
+    """Inside directory.
+
+    Recursively walks both the "prior" and "cur" directories,
+    comparing the trees found inside.  Returns when each has left this
+    directory."""
+
+    a = prior.next()
+    b = cur.next()
+    while True:
+
+	# print "Comparing (%d) %s and %s" % (depth, a, b)
+
+	# Both are 'leave' nodes.
+	if a[0] == 'u' and b[0] == 'u':
+	    # print "...leave"
+	    return
+
+	if a[0] == 'd' and b[0] == 'd':
+	    # Both are looking at a child subdirectory.
+
+	    if a[1] == b[1]:
+		# Same name, just walk this tree.
+		# print "...same dir, enter"
+		comp_walk(prior, cur, depth+1)
+		a = prior.next()
+		b = cur.next()
+		continue
+
+	    elif a[1] < b[1]:
+		# A directory has been deleted.
+		print "Delete dir: %s" % a[1]
+		consume_dir(prior)
+		a = prior.next()
+		continue
+
+	    else:
+		# A directory has been added.
+		print "Add dir: %s" % b[1]
+		consume_dir(cur)
+		b = cur.next()
+		continue
+
+	elif a[0] == '-' and b[0] == '-':
+	    # Both are looking at a non-dir.
+	    if a[1] == b[1]:
+		# Same name, all is well.
+		# print "...same file"
+		a = prior.next()
+		b = cur.next()
+		continue
+
+	    elif a[1] < b[1]:
+		print "Delete nondir: %s" % a[1]
+		a = prior.next()
+		continue
+
+	    else:
+		print "Add nondir: %s" % b[1]
+		b = cur.next()
+		continue
+
+	elif a[0] == '-' and b[0] == 'u':
+	    print "Delete nondir: %s" % a[1]
+	    a = prior.next()
+	    continue
+
+	elif a[0] == 'u' and b[0] == '-':
+	    print "New nondir: %s" % b[1]
+	    b = cur.next()
+	    continue
+
+	elif a[0] == 'd' and b[0] == '-':
+	    print "Delete dir: %s" % a[1]
+	    consume_dir(prior)
+	    a = prior.next()
+	    continue
+
+	elif (a[0] == '-' or a[0] == 'u') and b[0] == 'd':
+	    print "Add dir: %s" % b[1]
+	    consume_dir(cur)
+	    b = cur.next()
+	    continue
+
+	else:
+	    print "Unhandled case: prior: %s, cur: %s" % (a[0], b[0])
+	    sys.exit(2)
+
+def consume_dir(iter):
+    """Consume entries until this directory has been exhausted"""
+    while True:
+	a = iter.next()
+	if a[0] == 'u':
+	    return
+	elif a[0] == 'd':
+	    consume_dir(iter)
+
 version = 'Asure scan version 1.0'
 
 def reader(path):
@@ -85,6 +195,13 @@ def fresh_scan():
     """Perform a fresh scan of the filesystem"""
     writer('asure.dat.gz', 'asure.0.gz', walk('.'))
 
+def check_scan():
+    """Perform a scan of the filesystem, and compare it with the scan
+    file.  reports differences."""
+    prior = reader('asure.dat.gz')
+    cur = walk('.')
+    compare_trees(prior, cur)
+
 def main(argv):
     if len(argv) != 1:
 	usage()
@@ -93,7 +210,7 @@ def main(argv):
     elif argv[0] == 'update':
 	print "Update"
     elif argv[0] == 'check':
-	print "Check"
+	check_scan()
     elif argv[0] == 'show':
 	for i in reader('asure.dat.gz'):
 	    print i
